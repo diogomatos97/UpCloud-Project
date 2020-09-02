eeb096ffaa834455a7cfa1b2178c4d64ef5d5d81
diogomatos97 <diogopsnskullcrap@gmail.com>
2020-09-02 12:38:38 +0100

    Added Login Function

---
+++ Project/UpCloudLogic/Login.cs
@@ -0,0 +1,28 @@
+﻿using Project;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UpCloudLogic
+{
+    class Login
+    {
+        public bool LoginCheck(string username, string password)
+        {
+            using (var db = new ProjectContext())
+            {
+                var login = db.Manager.Where(c => c.Username == username.GetHashCode().ToString() && c.Password == password.GetHashCode().ToString()).FirstOrDefault();
+                if (login != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
+    }
+}
